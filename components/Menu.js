4d577874e23d43cbd2f2fa502669310c74cbbdf9
Márcio Brasil <marcio@mbrasil.net>
2020-12-20 17:48:09 +0000

    sub menus sociais

--- components/Menu.js
+++ components/Menu.js
@@ -9,12 +9,12 @@ const Menu = () => {
         <div>
             <Navbar bg="" expand="lg">
                 <Navbar.Brand href="/">
-                    <Image 
-                        src="/logo-mbrasil.png" 
+                    <Image
+                        src="/logo-mbrasil.png"
                         layout="fixed"
-                        width={200} 
-                        height={57} 
-                        alt="MBrasil" 
+                        width={200}
+                        height={57}
+                        alt="MBrasil"
                         q={25} />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -23,6 +23,12 @@ const Menu = () => {
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About Us</Nav.Link>
                         <Nav.Link href="/contact">Contact</Nav.Link>
+                        <NavDropdown title="Social" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://www.linkedin.com/in/mbrasil/" target="_blank">Linkedin</NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.youtube.com/user/WebDeveloperBrasil" target="_blank">YouTube</NavDropdown.Item>
+                            <NavDropdown.Item href="https://github.com/mbrasilce" target="_blank">GitHub</NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.facebook.com/ProfMarcioBrasil" target="_blank">Facebook</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
