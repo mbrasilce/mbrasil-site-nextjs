1c4bebdb6f6a43714413fea6a03fef26eb907460
Márcio Brasil <marcio@mbrasil.net>
2021-02-16 06:45:54 +0000

    Correçao de textos

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,66 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Menu from '../components/Menu'
-import Footer from '../components/Footer'
+import Menu from "../components/Menu";
+import Footer from "../components/Footer";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Link from 'next/link';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
 
-import useSWR from 'swr'
-import Contenthome from '../components/Contenthome'
-const fetcher = (url) => fetch(url).then((res) => res.json())
+import useSWR from "swr";
+import Contenthome from "../components/Contenthome";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 function Home() {
+  const { data, error } = useSWR("/api/contenthome", fetcher);
 
-    const { data, error } = useSWR('/api/contenthome', fetcher)
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
-    if (error) return <div>Failed to load</div>
-    if (!data) return <div>Loading...</div>
+  return (
+    <div className="container">
+      <Head>
+        <title>MBrasil - Desenvolvimento de Websites</title>
+      </Head>
+      <Menu></Menu>
+      <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 className="display-4">Web Developer</h1>
+        <p className="lead">
+          Se estava a buscar um desenvolvedor web, sua procura acabou. Sites
+          profissionais, rápidos, simples e baratos.
+        </p>
+      </div>
 
-    return (
-
-        <div className="container">
-
-            <Head>
-                <title>MBrasil - Desenvolvimento de Websites</title>
-            </Head>
-            <Menu></Menu>
-            <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 className="display-4">Web Developer</h1>
-                <p className="lead">Se estava a buscar um desenvolvedor web, sua procura acabou. Sites profissionais, rápidos, simples e baratos.</p>
-            </div>
-
-            <div className="container">
-                <div className="card-deck mb-3 text-center">
-                    {data
-                    .filter(sites => sites['tipo'] == 'sites')
-                    .map((p, i) => (
-                        <Contenthome key={i} sites={p} />
-                    ))}    
-                </div>
-            </div>
-
-            <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 className="display-4">Suporte e Hospedagem</h1>
-                <p className="lead">Hospedagem profissinal com suporte integrado e personalizado. Registro de domínios e configuração de emails e sies.</p>
-            </div>
-
-            <div className="container">
-                <div className="card-deck mb-3 text-center">
-                    {data
-                    .filter(sites => sites['tipo'] == 'hospedagem')
-                    .map((p, i) => (
-                        <Contenthome key={i} sites={p} />
-                    ))}    
-                </div>
-            </div>
-
-            <Footer></Footer>
+      <div className="container">
+        <div className="card-deck mb-3 text-center">
+          {data
+            .filter((sites) => sites["tipo"] == "sites")
+            .map((p, i) => (
+              <Contenthome key={i} sites={p} />
+            ))}
         </div>
-    )
+      </div>
+
+      <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 className="display-4">Suporte e Hospedagem</h1>
+        <p className="lead">
+          Alojamento profissional com suporte integrado e personalizado.
+          Registro de domínios e configuração de emails e sites.
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="card-deck mb-3 text-center">
+          {data
+            .filter((sites) => sites["tipo"] == "hospedagem")
+            .map((p, i) => (
+              <Contenthome key={i} sites={p} />
+            ))}
+        </div>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
